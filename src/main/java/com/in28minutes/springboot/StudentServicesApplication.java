fbcbfde6a0b221ba8fca0fa6f6bf7ac702d1a445
David Ayers <davidaayers@gmail.com>
2018-08-26 10:24:07 -0600

    Format code according to proper code style guidelines
    The initial code commit included code that wasn't formatted according
    to our coding standards.

--- src/main/java/com/in28minutes/springboot/StudentServicesApplication.java
+++ src/main/java/com/in28minutes/springboot/StudentServicesApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentServicesApplication {
+public class StudentServicesApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentServicesApplication.class, args);
-	}
+   public static void main(String[] args)
+   {
+      SpringApplication.run(StudentServicesApplication.class, args);
+   }
 }
